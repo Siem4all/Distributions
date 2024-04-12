432619ddabe02555b68d7881eaf80e159aa057c0
Siem Gebrehiwet <102214627+Siem4all@users.noreply.github.com>
2024-04-12 11:13:48 +0300

    Update ResNet.py

--- ResNet18.py
+++ ResNet18.py
@@ -1,285 +1,203 @@
-import argparse
-import os
-import shutil
-import time
+import numpy as np
 import torch
-import torchvision.datasets as datasets
-import torchvision.transforms as transforms
+import torch.nn as nn
+from torchvision import datasets
+from torchvision import transforms
+from torch.utils.data.sampler import SubsetRandomSampler
 
-from torchvision.models.resnet import resnet18
-
-from pytorch_nndct import Pruner
-
-# https://docs.amd.com/r/2.0-English/ug1333-ai-optimizer/ResNet18
-parser = argparse.ArgumentParser()
-parser.add_argument(
-    '--data_dir',
-    default='/scratch/workspace/dataset/imagenet/pytorch',
-    help='Data set directory.')
-parser.add_argument(
-    '--pretrained',
-    default='/scratch/workspace/models/resnet18.pth',
-    help='Trained model file path.')
-parser.add_argument(
-    '--ratio',
-    default=0.1,
-    type=float,
-    help='Desired pruning ratio. The larger this value, the smaller'
-         'the model after pruning.')
-parser.add_argument(
-    '--ana',
-    default=False,
-    type=bool,
-    help='Whether to perform model analysis.')
-args, _ = parser.parse_known_args()
+# Device configuration
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 
-class AverageMeter(object):
-    """Computes and stores the average and current value"""
+def data_loader(data_dir,
+                batch_size,
+                random_seed=42,
+                valid_size=0.1,
+                shuffle=True,
+                test=False):
+    normalize = transforms.Normalize(
+        mean=[0.4914, 0.4822, 0.4465],
+        std=[0.2023, 0.1994, 0.2010],
+    )
 
-    def __init__(self, name, fmt=':f'):
-        self.name = name
-        self.fmt = fmt
-        self.reset()
+    # define transforms
+    transform = transforms.Compose([
+        transforms.Resize((224, 224)),
+        transforms.ToTensor(),
+        normalize,
+    ])
 
-    def reset(self):
-        self.val = 0
-        self.avg = 0
-        self.sum = 0
-        self.count = 0
+    if test:
+        dataset = datasets.CIFAR10(
+            root=data_dir, train=False,
+            download=True, transform=transform,
+        )
 
-    def update(self, val, n=1):
-        self.val = val
-        self.sum += val * n
-        self.count += n
-        self.avg = self.sum / self.count
+        data_loader = torch.utils.data.DataLoader(
+            dataset, batch_size=batch_size, shuffle=shuffle
+        )
 
-    def __str__(self):
-        fmtstr = '{name} {val' + self.fmt + '} ({avg' + self.fmt + '})'
-        return fmtstr.format(**self.__dict__)
+        return data_loader
 
+    # load the dataset
+    train_dataset = datasets.CIFAR10(
+        root=data_dir, train=True,
+        download=True, transform=transform,
+    )
 
-def save_checkpoint(state, is_best, filename='checkpoint.pth.tar'):
-    torch.save(state, filename)
-    if is_best:
-        shutil.copyfile(filename, 'model_best.pth.tar')
+    valid_dataset = datasets.CIFAR10(
+        root=data_dir, train=True,
+        download=True, transform=transform,
+    )
 
+    num_train = len(train_dataset)
+    indices = list(range(num_train))
+    split = int(np.floor(valid_size * num_train))
 
-class ProgressMeter(object):
+    if shuffle:
+        np.random.seed(42)
+        np.random.shuffle(indices)
 
-    def __init__(self, num_batches, meters, prefix=""):
-        self.batch_fmtstr = self._get_batch_fmtstr(num_batches)
-        self.meters = meters
-        self.prefix = prefix
+    train_idx, valid_idx = indices[split:], indices[:split]
+    train_sampler = SubsetRandomSampler(train_idx)
+    valid_sampler = SubsetRandomSampler(valid_idx)
 
-    def display(self, batch):
-        entries = [self.prefix + self.batch_fmtstr.format(batch)]
-        entries += [str(meter) for meter in self.meters]
-        print('\t'.join(entries))
+    train_loader = torch.utils.data.DataLoader(
+        train_dataset, batch_size=batch_size, sampler=train_sampler)
 
-    def _get_batch_fmtstr(self, num_batches):
-        num_digits = len(str(num_batches // 1))
-        fmt = '{:' + str(num_digits) + 'd}'
-        return '[' + fmt + '/' + fmt.format(num_batches) + ']'
+    valid_loader = torch.utils.data.DataLoader(
+        valid_dataset, batch_size=batch_size, sampler=valid_sampler)
 
-
-def accuracy(output, target, topk=(1,)):
-    """Computes the accuracy over the k top predictions
-    for the specified values of k"""
-    with torch.no_grad():
-        maxk = max(topk)
-        batch_size = target.size(0)
-
-        _, pred = output.topk(maxk, 1, True, True)
-        pred = pred.t()
-        correct = pred.eq(target.view(1, -1).expand_as(pred))
-
-        res = []
-        for k in topk:
-            correct_k = correct[:k].view(-1).float().sum(0, keepdim=True)
-            res.append(correct_k.mul_(100.0 / batch_size))
-        return res
+    return (train_loader, valid_loader)
 
 
-def adjust_learning_rate(optimizer, epoch, lr):
-    """Sets the learning rate to the initial LR decayed by every 2 epochs"""
-    lr = lr * (0.1 ** (epoch // 2))
+# CIFAR10 dataset
+train_loader, valid_loader = data_loader(data_dir='./data',
+                                         batch_size=64)
 
-    for param_group in optimizer.param_groups:
-        param_group['lr'] = lr
+test_loader = data_loader(data_dir='./data',
+                          batch_size=64,
+                          test=True)
 
 
-def train(train_loader, model, criterion, optimizer, epoch):
-    batch_time = AverageMeter('Time', ':6.3f')
-    data_time = AverageMeter('Data', ':6.3f')
-    losses = AverageMeter('Loss', ':.4e')
-    top1 = AverageMeter('Acc@1', ':6.2f')
-    top5 = AverageMeter('Acc@5', ':6.2f')
-    progress = ProgressMeter(
-        len(train_loader), [batch_time, data_time, losses, top1, top5],
-        prefix="Epoch: [{}]".format(epoch))
+class ResidualBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, stride=1, downsample=None):
+        super(ResidualBlock, self).__init__()
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=stride, padding=1),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU())
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(out_channels))
+        self.downsample = downsample
+        self.relu = nn.ReLU()
+        self.out_channels = out_channels
 
-    # switch to train mode
-    model.train()
+    def forward(self, x):
+        residual = x
+        out = self.conv1(x)
+        out = self.conv2(out)
+        if self.downsample:
+            residual = self.downsample(x)
+        out += residual
+        out = self.relu(out)
+        return out
 
-    end = time.time()
-    for i, (images, target) in enumerate(train_loader):
-        # measure data loading time
-        data_time.update(time.time() - end)
 
-        model = model.cuda()
-        images = images.cuda()
-        target = target.cuda()
+class ResNet(nn.Module):
+    def __init__(self, block, layers, num_classes=10):
+        super(ResNet, self).__init__()
+        self.inplanes = 64
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3),
+            nn.BatchNorm2d(64),
+            nn.ReLU())
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        self.layer0 = self._make_layer(block, 64, layers[0], stride=1)
+        self.layer1 = self._make_layer(block, 128, layers[1], stride=2)
+        self.layer2 = self._make_layer(block, 256, layers[2], stride=2)
+        self.layer3 = self._make_layer(block, 512, layers[3], stride=2)
+        self.avgpool = nn.AvgPool2d(7, stride=1)
+        self.fc = nn.Linear(512, num_classes)
 
-        # compute output
-        output = model(images)
-        loss = criterion(output, target)
+    def _make_layer(self, block, planes, blocks, stride=1):
+        downsample = None
+        if stride != 1 or self.inplanes != planes:
+            downsample = nn.Sequential(
+                nn.Conv2d(self.inplanes, planes, kernel_size=1, stride=stride),
+                nn.BatchNorm2d(planes),
+            )
+        layers = []
+        layers.append(block(self.inplanes, planes, stride, downsample))
+        self.inplanes = planes
+        for i in range(1, blocks):
+            layers.append(block(self.inplanes, planes))
 
-        # measure accuracy and record loss
-        acc1, acc5 = accuracy(output, target, topk=(1, 5))
-        losses.update(loss.item(), images.size(0))
-        top1.update(acc1[0], images.size(0))
-        top5.update(acc5[0], images.size(0))
+        return nn.Sequential(*layers)
 
-        # compute gradient and do SGD step
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.maxpool(x)
+        x = self.layer0(x)
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+
+        x = self.avgpool(x)
+        x = x.view(x.size(0), -1)
+        x = self.fc(x)
+
+        return x
+
+
+num_classes = 10
+num_epochs = 20
+batch_size = 16
+learning_rate = 0.01
+
+model = ResNet(ResidualBlock, [3, 4, 6, 3]).to(device)
+
+# Loss and optimizer
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate, weight_decay=0.001, momentum=0.9)
+
+# Train the model
+total_step = len(train_loader)
+
+import gc
+
+total_step = len(train_loader)
+
+for epoch in range(num_epochs):
+    for i, (images, labels) in enumerate(train_loader):
+        # Move tensors to the configured device
+        images = images.to(device)
+        labels = labels.to(device)
+
+        # Forward pass
+        outputs = model(images)
+        loss = criterion(outputs, labels)
+
+        # Backward and optimize
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
-
-        # measure elapsed time
-        batch_time.update(time.time() - end)
-        end = time.time()
-
-        if i % 10 == 0:
-            progress.display(i)
-
-
-def evaluate(val_loader, model, criterion):
-    batch_time = AverageMeter('Time', ':6.3f')
-    losses = AverageMeter('Loss', ':.4e')
-    top1 = AverageMeter('Acc@1', ':6.2f')
-    top5 = AverageMeter('Acc@5', ':6.2f')
-    progress = ProgressMeter(
-        len(val_loader), [batch_time, losses, top1, top5], prefix='Test: ')
-
-    # switch to evaluate mode
-    model.eval()
-
+        del images, labels, outputs
+        torch.cuda.empty_cache()
+        gc.collect()
+        
+    # Validation
     with torch.no_grad():
-        end = time.time()
-        for i, (images, target) in enumerate(val_loader):
-            model = model.cuda()
-            images = images.cuda(non_blocking=True)
-            target = target.cuda(non_blocking=True)
+        correct = 0
+        total = 0
+        for images, labels in valid_loader:
+            images = images.to(device)
+            labels = labels.to(device)
+            outputs = model(images)
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+            del images, labels, outputs
 
-            # compute output
-            output = model(images)
-            loss = criterion(output, target)
-
-            # measure accuracy and record loss
-            acc1, acc5 = accuracy(output, target, topk=(1, 5))
-            losses.update(loss.item(), images.size(0))
-            top1.update(acc1[0], images.size(0))
-            top5.update(acc5[0], images.size(0))
-
-            # measure elapsed time
-            batch_time.update(time.time() - end)
-            end = time.time()
-
-            if i % 50 == 0:
-                progress.display(i)
-
-        # TODO: this should also be done with the ProgressMeter
-        print(' * Acc@1 {top1.avg:.3f} Acc@5 {top5.avg:.3f}'.format(
-            top1=top1, top5=top5))
-
-    return top1.avg, top5.avg
-
-
-# Define a function used for model analysis
-
-def ana_eval_fn(model, val_loader, loss_fn):
-    return evaluate(val_loader, model, loss_fn)[1]
-
-
-# Create a ResNet18 model and add the pruning APIs to perform pruning.
-
-if __name__ == '__main__':
-    model = resnet18().cpu()
-    model.load_state_dict(torch.load(args.pretrained))
-
-    batch_size = 128
-    workers = 4
-    traindir = os.path.join(args.data_dir, 'train')
-    valdir = os.path.join(args.data_dir, 'validation')
-
-    normalize = transforms.Normalize(
-        mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
-
-    train_dataset = datasets.ImageFolder(
-        traindir,
-        transforms.Compose([
-            transforms.RandomResizedCrop(224),
-            transforms.RandomHorizontalFlip(),
-            transforms.ToTensor(),
-            normalize,
-        ]))
-
-    train_loader = torch.utils.data.DataLoader(
-        train_dataset,
-        batch_size=batch_size,
-        shuffle=True,
-        num_workers=workers,
-        pin_memory=True)
-
-    val_dataset = datasets.ImageFolder(
-        valdir,
-        transforms.Compose([
-            transforms.Resize(256),
-            transforms.CenterCrop(224),
-            transforms.ToTensor(),
-            normalize,
-        ]))
-
-    val_loader = torch.utils.data.DataLoader(
-        val_dataset,
-        batch_size=batch_size,
-        shuffle=False,
-        num_workers=workers,
-        pin_memory=True)
-
-    criterion = torch.nn.CrossEntropyLoss().cuda()
-
-    inputs = torch.randn([1, 3, 224, 224], dtype=torch.float32)
-    pruner = Pruner(model, inputs)
-    if args.ana:
-        pruner.ana(ana_eval_fn, args=(val_loader, criterion), gpus=[0, 1, 2, 3])
-    model = pruner.prune(ratio=args.ratio)
-    pruner.summary(model)
-
-    lr = 1e-4
-    optimizer = torch.optim.Adam(model.parameters(), lr, weight_decay=1e-4)
-
-    best_acc5 = 0
-    epochs = 1
-    for epoch in range(epochs):
-        adjust_learning_rate(optimizer, epoch, lr)
-
-        train(train_loader, model, criterion, optimizer, epoch)
-
-        acc1, acc5 = evaluate(val_loader, model, criterion)
-
-        # remember best acc@1 and save checkpoint
-        is_best = acc5 > best_acc5
-        best_acc5 = max(acc5, best_acc5)
-
-        if is_best:
-            torch.save(model.pruned_state_dict(), 'resnet18_sparse.pth')
-            torch.save(model.state_dict(), 'resnet18_dense.pth')
-# Download pretrained ResNet18 model:
-
-# Run the first round of pruning with model analysis:
-# $ python -u resnet18_pruning.py --data_dir imagenet_dir --pretrained resnet18.pth --ratio 0.1  --ana True
-
-# From the second round onwards, model analysis is no longer required. Increase the pruning ratio and use the sparse checkpoint saved from previous round as the pretrained weights.
-# $ python -u resnet18_pruning.py --data_dir imagenet_dir --pretrained resnet18_sparse.pth.tar --ratio 0.2
+        print('Accuracy of the network on the {} validation images: {} %'.format(5000, 100 * correct / total))
